3667124a76a4dae770efef4dc81a8438bb443193
Ad-closeNN <1709301095@qq.com>
2024-11-08 21:56:38 +0800

    不用pytz了 :+1:

--- Quick Python File Creater.py
+++ Quick Python File Creater.py
@@ -1,17 +1,20 @@
-from datetime import datetime
-import pytz
+from datetime import datetime, timedelta, timezone
 import os
+
 def get_time():
     global file_name
-    utc_plus_8 = datetime.now(pytz.timezone('Asia/Shanghai'))
+    utc_plus_8 = datetime.now(timezone(timedelta(hours=8)))  # 设置时区偏移为 UTC+8
     file_name = utc_plus_8.strftime("%Y-%m-%d_%H-%M-%S")
+
 def get_dir_path():
     global temp_path
     temp_path = os.getenv("TEMP")
+
 def create_test_file():
-    with open(temp_path+"\\"+file_name+".py", "w", encoding="utf-8") as test_file:
+    with open(temp_path + "\\" + file_name + ".py", "w", encoding="utf-8") as test_file:
         test_file.write("")
-    os.startfile(temp_path+"\\"+file_name+".py")
+    os.startfile(temp_path + "\\" + file_name + ".py")
+
 if __name__ == "__main__":
     os.system("cls")
     get_time()
